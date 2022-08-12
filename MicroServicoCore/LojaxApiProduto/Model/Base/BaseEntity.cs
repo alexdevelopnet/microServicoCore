66196f0d30cd3ca9a8d38b14999f3f690f537a28
FamiliaFeitoza <FamiliaFeitoza@FEITOZA>
2022-08-12 10:21:36 -0300

    Repository e Services finalizadas

---
+++ MicroServicoCore/LojaxApiProduto/Model/Base/BaseEntity.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaxApiProduto.Model.Base
+{
+    public class BaseEntity
+    {
+        
+        [Key]
+        [Column("id")]
+         public long Id { get; set; } 
+    }
+}
